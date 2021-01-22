4021037164b2fa28ea853ad62b79ead68055e45c
Misaka13514 <Misaka13514@gmail.com>
2021-01-22 12:45:32 +0800

    きれいになった

--- docs/js/base.js
+++ docs/js/base.js
@@ -1,24 +1,21 @@
-function getSearchTerm()
-{
+function getSearchTerm() {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
+    for (var i = 0; i < sURLVariables.length; i++) {
         var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == 'q')
-        {
+        if (sParameterName[0] == 'q') {
             return sParameterName[1];
         }
     }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     var search_term = getSearchTerm(),
         $search_modal = $('#mkdocs_search_modal'),
         $keyboard_modal = $('#mkdocs_keyboard_modal');
 
-    if(search_term){
+    if (search_term) {
         $search_modal.modal();
     }
 
@@ -28,27 +25,28 @@ $(document).ready(function() {
     });
 
     // Keyboard navigation
-    document.addEventListener("keydown", function(e) {
+    document.addEventListener("keydown", function (e) {
         if ($(e.target).is(':input')) return true;
         var key = e.which || e.keyCode || window.event && window.event.keyCode;
         var page;
         switch (key) {
-            case 39:   // right arrow
+            case 39: // right arrow
                 page = $('[role="navigation"] a:contains(Next):first').prop('href');
                 break;
-            case 37:   // left arrow
+            case 37: // left arrow
                 page = $('[role="navigation"] a:contains(Previous):first').prop('href');
                 break;
-            case 83:   // s
+            case 83: // s
                 e.preventDefault();
                 $keyboard_modal.modal('hide');
                 $search_modal.modal('show');
                 $search_modal.find('#mkdocs-search-query').focus();
                 break;
-            case 191:  // ?
+            case 191: // ?
                 $keyboard_modal.modal('show');
                 break;
-            default: break;
+            default:
+                break;
         }
         if (page) {
             $keyboard_modal.modal('hide');
@@ -61,9 +59,9 @@ $(document).ready(function() {
     $('table').addClass('table table-striped table-hover');
 
     // Improve the scrollspy behaviour when users click on a TOC item.
-    $(".bs-sidenav a").on("click", function() {
+    $(".bs-sidenav a").on("click", function () {
         var clicked = this;
-        setTimeout(function() {
+        setTimeout(function () {
             var active = $('.nav li.active a');
             active = active[active.length - 1];
             if (clicked !== active) {
@@ -82,11 +80,11 @@ $('body').scrollspy({
 
 /* Toggle the `clicky` class on the body when clicking links to let us
    retrigger CSS animations. See ../css/base.css for more details. */
-$('a').click(function(e) {
+$('a').click(function (e) {
     $('body').toggleClass('clicky');
 });
 
 /* Prevent disabled links from causing a page reload */
-$("li.disabled a").click(function() {
+$("li.disabled a").click(function () {
     event.preventDefault();
 });
